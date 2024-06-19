1ac52d4e050c91ce76b7a207ed4c51540693906a
devCarlosSantoss <carlosvinicius357890@gmail.com>
2024-06-18 22:57:30 -0300

    feat: Order status enum created

--- src/main/java/com/devcarlossantos/shoppingcartapi/config/TestConfig.java
+++ src/main/java/com/devcarlossantos/shoppingcartapi/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.devcarlossantos.shoppingcartapi.config;
 
 import com.devcarlossantos.shoppingcartapi.entities.Order;
 import com.devcarlossantos.shoppingcartapi.entities.User;
+import com.devcarlossantos.shoppingcartapi.entities.enums.OrderStatus;
 import com.devcarlossantos.shoppingcartapi.repositories.OrderRepository;
 import com.devcarlossantos.shoppingcartapi.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "71999249874", "123456");
         User user2 = new User(null, "Alex Green", "alexgreen@gmail.com", "71991235863", "123456");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.SHIPPED, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, user1);
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
